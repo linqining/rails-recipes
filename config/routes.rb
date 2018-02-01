d8646a73b4d0ba744e0cd76bf47bcdf280a5d2d8
linqining <linqining@outlook.com>
2018-02-01 17:17:16 +0800

    white list

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,18 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-   resources :events do
-     resources :tickets, :controller => "event_tickets"
-   end
+  resources :events do
+    resources :tickets, :controller => "event_tickets"
+  end
 
   namespace :admin do
     root "events#index"
     resources :events
     resources :users do
       resource :profile, :controller => "user_profiles"
+    end
+    resources :events do
+      resources :tickets, :controller => "event_tickets"
     end
   end
 
